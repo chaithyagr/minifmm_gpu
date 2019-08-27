d3653b0c147eab9906d5b01345303031293b0669
chaithyagr <chaithyagr@gmail.com>
2019-08-27 14:28:27 +0200

    Updates to codes, pull as updated submodule

--- src/tree.cpp
+++ src/tree.cpp
@@ -9,12 +9,6 @@
 #include "util.h"
 #include "tree.h"
 
-//#ifndef CACHELINE_SZ
-//#define CACHLINE_SZ 64
-//#endif
-//
-//#define CACHELINE_PADDING_SZ(x) ((CACHLINE_SZ - (x % CACHLINE_SZ)) % CACHLINE_SZ)
-
 void bound_box(TYPE* mins, TYPE* maxs, TYPE* points, size_t num_points)
 {
     for (int i = 0; i < 3; ++i) mins[i] = TYPE_MAX;
@@ -30,108 +24,7 @@ void bound_box(TYPE* mins, TYPE* maxs, TYPE* points, size_t num_points)
     }
 }
 
-//static size_t __num_node_allocations = 0;
-//static size_t __current_max_nodes = 0;
-//#define NODE_RECORD_GROWTH_FACTOR 1.5
-//
-//t_node** __node_array;
-//t_node** __node_arr_curr;
-//
-//size_t record_node(t_node* node)
-//{
-//    if (__num_node_allocations == __current_max_nodes)
-//    {
-//        // increase size of node records
-//        __current_max_nodes = (double)__current_max_nodes * NODE_RECORD_GROWTH_FACTOR;
-//        //printf("expanding max nodes to %zu\n", __current_max_nodes);
-//        __node_array = (t_node**)realloc(__node_array, sizeof(t_node*)*__current_max_nodes);
-//        __node_arr_curr = __node_array + __num_node_allocations - 1;
-//
-//        if (__node_array == NULL)
-//        {
-//            fprintf(stderr, "error: could not allocate enough tree nodes\n"); 
-//            exit(1);
-//        }
-//    }
-//    *__node_arr_curr = node;
-//    size_t ret = __node_arr_curr - __node_array;
-//    __node_arr_curr++;
-//    return ret;
-//}
-//
-//void init_node_array(t_fmm_params* params)
-//{
-//    __current_max_nodes = (params->num_points/params->ncrit);
-//    __node_array = (t_node**)malloc(sizeof(t_node*)*__current_max_nodes);
-//    __node_arr_curr = __node_array;
-//    //printf("init allocating %zu node records\n", __current_max_nodes);
-//}
-
-//t_node* allocate_node(t_fmm_params* params, size_t pts_in_leaf, TYPE* points, TYPE* weights)
-//{
-//    size_t node_padding = CACHELINE_PADDING_SZ(sizeof(t_node));
-//    size_t node_alloc_sz = sizeof(t_node) + node_padding;
-//
-//    size_t point_padding = CACHELINE_PADDING_SZ(sizeof(TYPE)*pts_in_leaf);
-//    // 8 points for x, y, z, w, ax, ay, az, p
-//    size_t point_alloc_sz = 8*(sizeof(TYPE)*pts_in_leaf + point_padding);
-//
-//    size_t cmplx_sz = sizeof(TYPE);
-//    size_t multipole_padding = CACHELINE_PADDING_SZ(cmplx_sz * params->num_multipoles);
-//    // *4 because of local and multipole expansions - complex numbers as two seperate arrays
-//    size_t multipole_alloc_sz = 4*(cmplx_sz*params->num_multipoles + multipole_padding);
-//
-//    size_t alloc_sz = node_alloc_sz + multipole_alloc_sz + point_alloc_sz;
-//
-//    //TODO change to own alloc function
-//    char* mem = (char*)_mm_malloc(alloc_sz, 64);
-//    memset(mem, 0, alloc_sz);
-//
-//    t_node* node = (t_node*)mem;
-//
-//    TYPE* mult_mem = (TYPE*)(&mem[node_alloc_sz]);
-//
-//    node->M_real = mult_mem;
-//    node->M_imag = (TYPE*)((char*)mult_mem + 1*(params->num_multipoles*cmplx_sz + multipole_padding));
-//    node->L_real = (TYPE*)((char*)mult_mem + 2*(params->num_multipoles*cmplx_sz + multipole_padding));
-//    node->L_imag = (TYPE*)((char*)mult_mem + 3*(params->num_multipoles*cmplx_sz + multipole_padding));
-//
-//    if (pts_in_leaf > 0)
-//    {
-//        TYPE* point_mem = (TYPE*)&mem[node_alloc_sz + multipole_alloc_sz];
-//        node->x  = point_mem;
-//        node->y  = (TYPE*)((char*)point_mem + 1*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->z  = (TYPE*)((char*)point_mem + 2*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->w  = (TYPE*)((char*)point_mem + 3*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->ax = (TYPE*)((char*)point_mem + 4*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->ay = (TYPE*)((char*)point_mem + 5*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->az = (TYPE*)((char*)point_mem + 6*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//        node->p  = (TYPE*)((char*)point_mem + 7*(pts_in_leaf*sizeof(TYPE) + point_padding));
-//
-//        for (size_t i = 0; i < pts_in_leaf; ++i)
-//        {
-//            node->x[i] = points[0*params->num_points+i];
-//            node->y[i] = points[1*params->num_points+i];
-//            node->z[i] = points[2*params->num_points+i];
-//            node->w[i] = weights[i];
-//        }
-//    }
-//
-//    ++__num_node_allocations;
-//    node->arr_idx = record_node(node);
-//    return node;
-//}
-
-//void free_node(t_node* node)
-//{
-//    //TODO change to own alloc function
-//    _mm_free(node);
-//    --__num_node_allocations;
-//}
-
 static size_t point_head = 0;
-
-//size_t get_num_nodes() { return __num_node_allocations; }
 
 size_t construct_tree(TYPE cx, TYPE cy, TYPE cz, TYPE r, size_t start, size_t end, size_t ncrit, TYPE* points, TYPE* weights,
         TYPE* points_ordered, TYPE* weights_ordered, int *reorder, int *reorder_ordered, TYPE* acc, TYPE* pot,
@@ -165,7 +58,6 @@ size_t construct_tree(TYPE cx, TYPE cy, TYPE cz, TYPE r, size_t start, size_t en
     parent->center[2] = cz;
     parent->rad = r;
     parent->level = level;
-
     parent->num_points = end - start;
     parent->num_children = 0;
 
@@ -229,7 +121,6 @@ size_t construct_tree(TYPE cx, TYPE cy, TYPE cz, TYPE r, size_t start, size_t en
                     ncrit, points_ordered, weights_ordered, points, weights, reorder_ordered, reorder, acc, pot, !points_switched, num_points, level+1, params);
                 parent = &params->node_array[parent_idx];
                 parent->child[parent->num_children++] = child;
-                //dfs tree
             }
         }
     }
@@ -241,7 +132,7 @@ size_t construct_tree(TYPE cx, TYPE cy, TYPE cz, TYPE r, size_t start, size_t en
 //    int count = root->num_children;
 //    for (int i = 0; i < root->num_children; ++i)
 //    {
-//        count += count_desc_nodes(root->child[i]); 
+//        count += count_desc_nodes(root->child[i]);
 //    }
 //    root->num_desc_nodes = count;
 //    return count;
@@ -268,7 +159,7 @@ void build_tree(t_fmm_params* params)
     // need to add EPS for points that lie on border of node
     max_rad += TYPE_EPS;
 
-    params->root = construct_tree(center[0], center[1], center[2], max_rad, 0, params->num_points, params->ncrit, params->points, params->weights, 
+    params->root = construct_tree(center[0], center[1], center[2], max_rad, 0, params->num_points, params->ncrit, params->points, params->weights,
             params->points_ordered, params->weights_ordered, params->reorder, params->reorder_ordered, params->acc, params->pot, 0, params->num_points, 0, params);
 
     //printf("Tree has %zu nodes\n", params->num_nodes);
@@ -307,29 +198,3 @@ void build_tree(t_fmm_params* params)
 //    free(__node_array);
 //}
 
-//void tree_to_result(t_fmm_params* params)
-//{
-//    //printf("total %zu records\n", __node_arr_curr - __node_array);
-//
-//    //for (int i = 0; i < params->num_nodes; ++i)
-//    //{
-//    //    printf("node %d has %d desc nodes\n", i, __node_array[i]->num_desc_nodes);
-//    //}
-//
-//    size_t counter = 0;
-//    for (size_t i = 0; i < params->num_nodes; ++i)
-//    {
-//        t_node* node = __node_array[i];
-//        if (is_leaf(node))
-//        {
-//            for (size_t j = 0; j < node->num_points; ++j)
-//            {
-//                params->acc[0*params->num_points+counter] = node->ax[j];
-//                params->acc[1*params->num_points+counter] = node->ay[j];
-//                params->acc[2*params->num_points+counter] = node->az[j];
-//                params->pot[counter] = node->p[j];
-//                ++counter;
-//            }
-//        }
-//    }
-//}
